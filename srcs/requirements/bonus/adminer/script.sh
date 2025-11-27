0307712d859fb74a0c188506f87ebaecb204b5b2
kkuhn1994 <kevkin548@gmail.com>
2025-11-27 23:38:56 +0100

    wordpress blank

--- srcs/requirements/bonus/adminer/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker stop adminer
-docker rm adminer
-# docker build -t my-adminer .
-# docker run -d -p 8080:8080 --name adminer my-adminer
--- srcs/requirements/bonus/ftp/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export ROOT_PASSWORD=$(cat /run/secrets/root_password)
-
-useradd -m ftpuser
-echo "${ROOT_PASSWORD}"
-echo "ftpuser:${ROOT_PASSWORD}" | chpasswd
-
-exec pure-ftpd -P 0.0.0.0 -j -l unix -p 30000:30010
